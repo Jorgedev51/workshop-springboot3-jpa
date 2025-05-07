4ae1a3131f015ed574c3e8ec4d9de68c0ffc1927
Jorge Luiz <jorgdev50@gmail.com>
2025-05-06 23:45:18 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,9 +1,6 @@
 package com.educandoweb.course.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -24,9 +21,9 @@ public class TestConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		
+
 	}
 
 }
